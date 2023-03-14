3285097ed9c0a011aff4b2b67bd50d17fd68ddf7
SheikhJack <kealebogakido@gmail.com>
2023-03-14 17:03:45 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='card'>
+        <img src='./logo.png' alt='' className='card_img' />  
+        <div className='card_text'>
+          <h1 className='card_heading'>improve your front-end skills by building projects</h1>
+        </div>
+        <div className='card_detail'>
+          <p>Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
